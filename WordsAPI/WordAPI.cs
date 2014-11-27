c4e09123b29c88623c1f08976f73ba4ae63e5136
vdurise <vdurise@ncsu.edu>
2014-11-26 19:48:32 -0500

    Adding all code to Git

---
+++ WordsAPI/WordAPI.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WordsAPI
+{
+    public class WordAPI
+    {
+        //Variables
+        string[,] Matrix;
+       // Microsoft.Office.Interop.Word.Application WordCheckApp;
+        int i, j;
+        string one, two;
+        List<string>[] DictionariesAtoZ;
+        List<string> WordsFound_List;
+        // object myLock;
+        //Methods
+        public void MatrixInit()
+        {
+            //WordCheckApp = new Microsoft.Office.Interop.Word.Application();
+            DictionariesAtoZ = new List<string>[26];
+            //myLock = new object();
+            for (int i = 0; i < 26; i++)
+            {
+                DictionariesAtoZ[i] = File.ReadAllLines(System.IO.Directory.GetCurrentDirectory() + "\\Dictionary\\" + ((char)(65 + i)).ToString() + ".txt").Where(aa => aa.Length > 2).ToList<string>();
+            }
+            //     
+        }
+        public List<string> ProcessMatrix(string[,] myMatrix)
+        {
+            List<Thread> workerThreads = new List<Thread>();
+            // Stopwatch ssss = new Stopwatch();
+            //  ssss.Start();
+            Matrix = myMatrix;
+            WordsFound_List = new List<string>();
+            for (i = 0; i < 4; i++)
+            {
+                for (j = 0; j < 4; j++)
+                {
+                    // ssss.Start();
+                    Thread thread = new Thread(() => FindWords_Recursive(i, j, Matrix[i, j], i.ToString() + j + ":"));
+                    workerThreads.Add(thread);
+                    thread.Start();
+                    //   ssss.Stop();
+                    //    Console.WriteLine(i + ":" + j);
+                }
+            }
+            foreach (Thread thread in workerThreads)
+            {
+                thread.Join();
+            }
+            return WordsFound_List;
+        }
+
+        private void FindWords_Recursive(int i, int j, string formingWord, string visitedPath)
+        {
+            // Console.WriteLine(visitedPath);
+            if (formingWord.Contains("\\"))
+            {
+                int p = formingWord.IndexOf("\\");
+                one = formingWord.Substring(0, formingWord.Length - 2);
+                two = formingWord.Substring(0, formingWord.Length - 3) + formingWord.ElementAt(p + 1);
+            }
+            else { one = formingWord; two = string.Empty; }
+            if (!string.IsNullOrEmpty(formingWord) && formingWord.Length > 2 && DictionariesAtoZ[Convert.ToInt32(formingWord[0] - 65)].BinarySearch(formingWord) >= 0)
+            {
+                lock (WordsFound_List)
+                {
+                    WordsFound_List.Add(formingWord);
+                }
+            }
+            if (i + 1 < 4 && j + 0 < 4 && !visitedPath.Contains((i + 1).ToString() + (j + 0).ToString() + ":") && !string.IsNullOrEmpty(DictionariesAtoZ[Convert.ToInt32(formingWord[0] - 65)].Where(www => ((www.Length - www.Replace(formingWord, String.Empty).Length) / formingWord.Length) > 0).FirstOrDefault()))
+                FindWords_Recursive(i + 1, j + 0, formingWord + Matrix[i + 1, j + 0], visitedPath + (i + 1).ToString() + (j + 0).ToString() + ":");
+            if (i + 0 < 4 && j + 1 < 4 && !visitedPath.Contains((i + 0).ToString() + (j + 1).ToString() + ":") && !string.IsNullOrEmpty(DictionariesAtoZ[Convert.ToInt32(formingWord[0] - 65)].Where(www => ((www.Length - www.Replace(formingWord, String.Empty).Length) / formingWord.Length) > 0).FirstOrDefault()))
+                FindWords_Recursive(i + 0, j + 1, formingWord + Matrix[i + 0, j + 1], visitedPath + (i + 0).ToString() + (j + 1).ToString() + ":");
+            if (i + 1 < 4 && j + 1 < 4 && !visitedPath.Contains((i + 1).ToString() + (j + 1).ToString() + ":") && !string.IsNullOrEmpty(DictionariesAtoZ[Convert.ToInt32(formingWord[0] - 65)].Where(www => ((www.Length - www.Replace(formingWord, String.Empty).Length) / formingWord.Length) > 0).FirstOrDefault()))
+                FindWords_Recursive(i + 1, j + 1, formingWord + Matrix[i + 1, j + 1], visitedPath + (i + 1).ToString() + (j + 1).ToString() + ":");
+            if (i - 1 > -1 && j + 1 < 4 && !visitedPath.Contains((i - 1).ToString() + (j + 1).ToString() + ":") && !string.IsNullOrEmpty(DictionariesAtoZ[Convert.ToInt32(formingWord[0] - 65)].Where(www => ((www.Length - www.Replace(formingWord, String.Empty).Length) / formingWord.Length) > 0).FirstOrDefault()))
+                FindWords_Recursive(i - 1, j + 1, formingWord + Matrix[i - 1, j + 1], visitedPath + (i - 1).ToString() + (j + 1).ToString() + ":");
+            if (i - 1 > -1 && j + 0 < 4 && !visitedPath.Contains((i - 1).ToString() + (j + 0).ToString() + ":") && !string.IsNullOrEmpty(DictionariesAtoZ[Convert.ToInt32(formingWord[0] - 65)].Where(www => ((www.Length - www.Replace(formingWord, String.Empty).Length) / formingWord.Length) > 0).FirstOrDefault()))
+                FindWords_Recursive(i - 1, j + 0, formingWord + Matrix[i - 1, j + 0], visitedPath + (i - 1).ToString() + (j + 0).ToString() + ":");
+            if (i - 1 > -1 && j - 1 > -1 && !visitedPath.Contains((i - 1).ToString() + (j - 1).ToString() + ":") && !string.IsNullOrEmpty(DictionariesAtoZ[Convert.ToInt32(formingWord[0] - 65)].Where(www => ((www.Length - www.Replace(formingWord, String.Empty).Length) / formingWord.Length) > 0).FirstOrDefault()))
+                FindWords_Recursive(i - 1, j - 1, formingWord + Matrix[i - 1, j - 1], visitedPath + (i - 1).ToString() + (j - 1).ToString() + ":");
+            if (i + 0 < 4 && j - 1 > -1 && !visitedPath.Contains((i + 0).ToString() + (j - 1).ToString() + ":") && !string.IsNullOrEmpty(DictionariesAtoZ[Convert.ToInt32(formingWord[0] - 65)].Where(www => ((www.Length - www.Replace(formingWord, String.Empty).Length) / formingWord.Length) > 0).FirstOrDefault()))
+                FindWords_Recursive(i + 0, j - 1, formingWord + Matrix[i + 0, j - 1], visitedPath + (i + 0).ToString() + (j - 1).ToString() + ":");
+            if (i + 1 < 4 && j - 1 > -1 && !visitedPath.Contains((i + 1).ToString() + (j - 1).ToString() + ":") && !string.IsNullOrEmpty(DictionariesAtoZ[Convert.ToInt32(formingWord[0] - 65)].Where(www => ((www.Length - www.Replace(formingWord, String.Empty).Length) / formingWord.Length) > 0).FirstOrDefault()))
+                FindWords_Recursive(i + 1, j - 1, formingWord + Matrix[i + 1, j - 1], visitedPath + (i + 1).ToString() + (j - 1).ToString() + ":");
+
+        }
+    }
+}
---
+++ WordsTest/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordsTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WordsAPI.WordAPI wordAPI = new WordsAPI.WordAPI();
+            string[,] testMatrix = { { "R", "H", "P", "AN" }, { "M", "A", "E", "Y" }, { "E", "R", "G", "S" }, { "R", "R", "E", "D" } };
+            wordAPI.MatrixInit();
+            do
+            {
+                Console.WriteLine("16 upper case strings, each in a line");
+                for (int i = 0; i < 4; i++)
+                {
+                    for (int y = 0; y < 4; y++)
+                    {
+                        testMatrix[i, y] = Console.ReadLine();
+                    }
+                }
+                Console.WriteLine("3 High Value Words:");
+                string[] highValue = new string[] { "AN", "Y", "R" };
+
+                for (int qq = 0; qq < 3; qq++)
+                {
+                    highValue[qq] = Console.ReadLine();
+                }
+                Stopwatch ssss = new Stopwatch();
+                ssss.Start();
+                List<string> Results = wordAPI.ProcessMatrix(testMatrix);
+                ssss.Stop();
+                var DistinctResults = Results.Distinct().OrderByDescending(aaa => aaa.Length);
+                var list1 = DistinctResults.Where(www => ((www.Length - www.Replace(highValue[0], String.Empty).Length) / highValue[0].Length) > 0).OrderByDescending(aaaa => aaaa.Length).ToList<string>();
+                var list2 = DistinctResults.Where(www => ((www.Length - www.Replace(highValue[1], String.Empty).Length) / highValue[1].Length) > 0).OrderByDescending(aaaa => aaaa.Length).Except(list1).ToList<string>();
+                var list3 = DistinctResults.Where(www => ((www.Length - www.Replace(highValue[2], String.Empty).Length) / highValue[2].Length) > 0).OrderByDescending(aaaa => aaaa.Length).Except(list1).Except(list2).ToList<string>();
+                var otherResults = DistinctResults.Except(list1).Except(list2).Except(list3);
+                var maxCount = new List<Int32> { list1.Count, list2.Count, list3.Count };
+                int rs = maxCount.Max();
+                for (int rr = 0; rr < rs; rr++)
+                {
+                    Console.WriteLine("{0} {1} {2} {3}", list1.ElementAtOrDefault(rr), list2.ElementAtOrDefault(rr), list3.ElementAtOrDefault(rr), otherResults.ElementAtOrDefault(rr));
+                }
+                Console.WriteLine(ssss.Elapsed + "----" + DistinctResults.Count() + ";; Any key - repeat, else - escape");
+            }
+            while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+            // Console.ReadKey();
+        }
+    }
+}
